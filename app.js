5d86ead695bf00b099d43ccc598b0ebd66b67f17
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-31 05:55:02 +0300

    app.get / calls the routes/book.js routes /

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express    = require( 'express'             );
 const path       = require( 'path'                );
 const bodyParser = require( 'body-parser'         );
 
-const rootDir = require( './utilities/rootDir' );
+const rootDir    = require( './utilities/rootDir' );
+const bookRoutes = require( './routes/book' );
 
 const app = express();
 
@@ -13,8 +14,6 @@ app.set( 'views', 'views'     );
 app.use( express.static( path.join( rootDir, 'public' ) ) );
 app.use( bodyParser.urlencoded( { extended: false } )     );
 
-app.get( '/', ( req, res, next ) => {
-    res.render( 'test' );
-} );
+app.use( bookRoutes );
 
 app.listen( process.env.PORT || 3000 );
