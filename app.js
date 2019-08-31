3da34232b4d7135a5cfd900a149aec5e26a62cd2
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-31 05:14:46 +0300

    app.js -> some code formatting

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require( 'express' );
-const path = require( 'path' );
+const express = require( 'express'             );
+const path    = require( 'path'                );
 const rootDir = require( './utilities/rootDir' );
 
 const app = express();
 
 app.set( 'view engine', 'pug' );
-app.set( 'views', 'views' );
+app.set( 'views', 'views'     );
 
 
 app.use( express.static( path.join( rootDir, 'public' ) ) );
