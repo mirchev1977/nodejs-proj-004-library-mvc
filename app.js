cd720ba77600b6ba8def52c0d277c14e4cba73b4
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-31 04:40:01 +0300

    added npm start command and printed basic Hello World

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require( 'express' );
+
+const app = express();
+
+app.get( '/', ( req, res, next ) => {
+    res.write( '<h1>Hello World</h1>' );
+    res.end();
+} );
+
+app.listen( process.env.PORT || 3000 );
