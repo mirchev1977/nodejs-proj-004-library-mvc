2ed670a8416d7d67d8a4e889f866ecd8193e233f
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-08-31 05:01:13 +0300

    app.use( express.static( ...public))

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require( 'express' );
+const path = require( 'path' );
+const rootDir = require( './utilities/rootDir' );
 
 const app = express();
+app.use( express.static( path.join( rootDir, 'public' ) ) );
 
 app.get( '/', ( req, res, next ) => {
     res.write( '<h1>Hello World</h1>' );
