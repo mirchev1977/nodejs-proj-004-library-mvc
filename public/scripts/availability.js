b755815a7b94a6138cf2e09d97fae9abbbbcb9ec
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-05 05:04:23 +0300

    Fixed bug when checking loan and availability

--- public/scripts/availability.js
+++ public/scripts/availability.js
@@ -1,17 +1,32 @@
-var formBookShopAvailability = document.getElementById( 'form__book-shop' );
+var formsBookShopAvailability = document.getElementsByClassName( 'form__book-shop' );
 
 const errorMessage     = document.getElementById( 'error-message'            );
 errorMessage.style.display = "block";
 
-formBookShopAvailability.addEventListener( 'submit', function ( ev ) {
-    var amountToLoan    = document.getElementById( 'loan-amount'      );
-    var amountAvailable = document.getElementById( 'available-amount' );
+for ( var i in formsBookShopAvailability ) {
+    var formBookShopAvailability = formsBookShopAvailability[ i ];
 
-    var diff = amountAvailable.innerText - amountToLoan.value;
+    var isHtmlElement =
+        formBookShopAvailability instanceof HTMLElement;
 
-    if ( diff < 0 ) {
-        ev.preventDefault();
-        errorMessage.innerText = 'Book Amount loaned canneot be bigger than the amount of the available books.';
-        return false; 
-    }
-} );
+    if ( !isHtmlElement ) continue;
+
+
+    formBookShopAvailability.addEventListener( 'submit', function ( ev ) {
+        var amountAvailable = ev.target.parentElement
+            .parentElement.children[2].innerText;
+
+        var amountToLoan = ev.target.parentElement.parentElement
+            .children[4].children[0].children[2].value;
+
+
+        var diff = ( amountAvailable * 1) 
+            - ( amountToLoan * 1);
+
+        if ( diff < 0 ) {
+            ev.preventDefault();
+            errorMessage.innerText = 'Book Amount loaned canneot be bigger than the amount of the available books.';
+            return false; 
+        }
+    } );
+}
